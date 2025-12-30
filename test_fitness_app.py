ab46372e86264bd1a584be6c5952fa2b848abc5b
Niranjan  sheelavant <niranjansheelavant.ns@gmail.com>
2025-12-30 18:31:39 +0530

    Update and rename test_fitness.py to test_fitness_app.py

--- test_fitness_app.py
+++ test_fitness_app.py
@@ -4,12 +4,12 @@ def test_fitness_report():
     expected_output = (
         "----- Fitness Report -----\n"
         "Name            : Niranjan\n"
-        "Steps Taken     : 8000\n"
-        "Calories Burned : 500\n"
+        "Steps Taken     : 7000\n"
+        "Calories Burned : 400\n"
         "Workout Time    : 30 minutes\n"
-        "Fitness Score   : 160.00\n"
+        "Fitness Score   : 140.00\n"
         "Fitness Level   : Excellent"
     )
 
-    result = fitness_report("Niranjan", 8000, 500, 30)
+    result = fitness_report("Niranjan", 7000, 400, 30)
     assert result == expected_output
